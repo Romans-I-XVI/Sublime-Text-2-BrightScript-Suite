9ceafed1853a9ccaadd6bf5a972159999830274d
asojka09 <austinsojka@hotmail.com>
2015-11-18 10:10:41 -0600

    Added zipping and installing folder

--- install_to_roku.py
+++ install_to_roku.py
@@ -1,27 +1,36 @@
 import sublime, sublime_plugin
 import sys
 import os
+import zipfile
 import pycurl
 
 class InstallToRokuCommand(sublime_plugin.WindowCommand):
     def run(self, dirs):
-    	# sys.path.append(os.path.join(os.path.dirname(__file__), "/home/asojka09/.config/sublime-text-2/Packages/User/pycurl.egg"))
-    	# sys.path.remove(os.path.join(os.path.dirname(__file__), "/usr/lib/python2.7/site-packages"))
         if sublime.ok_cancel_dialog("Install To Roku?", "Install"):
+			folder = dirs[0]
+			zipfilename = "%s.zip" % (folder.replace("/", "_"))
+			zfile = zipfile.ZipFile("channel.zip", 'w', zipfile.ZIP_DEFLATED)
+			rootlen = len(folder) + 1
+			for base, dirs, files in os.walk(folder):
+			    for file in files:
+			        fn = os.path.join(base, file)
+			        zfile.write(fn, fn[rootlen:])
+			zfile.close()
+
 			username = "rokudev"
 			password = "password"
-			url = "192.168.0.43"+"/plugin_install"
-			package = "/home/asojka09/Development/Roku/installer-test/package.zip"
-
+			url = "192.168.1.10"+"/plugin_install"
 
 			curl = pycurl.Curl()
 			curl.setopt(pycurl.POST, 1)
 			curl.setopt(pycurl.URL, url)
-			curl.setopt(pycurl.HTTPPOST, [("mysubmit", "Install"), ("archive", (curl.FORM_FILE, package))])
+			curl.setopt(pycurl.HTTPPOST, [("mysubmit", "Install"), ("archive", (curl.FORM_FILE, "channel.zip"))])
 			curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
 			curl.setopt(pycurl.USERNAME, username)
 			curl.setopt(pycurl.PASSWORD, password)
 			curl.perform()
+
+			os.remove("channel.zip")
 
     def is_visible(self, dirs):
         return len(dirs) > 0
