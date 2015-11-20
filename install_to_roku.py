b59b44caea7543402ed67dad381d5137b5345524
asojka09 <austinsojka@hotmail.com>
2015-11-19 21:58:19 -0600

    Added keyboard shortcut

--- install_to_roku.py
+++ install_to_roku.py
@@ -10,8 +10,10 @@ class InstallToRokuCommand(sublime_plugin.WindowCommand):
 		ip = sublime.Settings.get(settings, "roku_ip_address")
 		url = "http://"+ip+"/plugin_install"
 		folder = dirs[0]
+		if not folder:
+			folder = sublime.Settings.get(settings, "roku_channel_path")
 
-		if sublime.ok_cancel_dialog("Install folder '"+dirs[0]+"' to Roku at "+ip+"?", "Install"):
+		if sublime.ok_cancel_dialog("Install folder '"+folder+"' to Roku at "+ip+"?", "Install"):
 			install = True
 
 			if not os.path.isfile(os.path.join(folder,"manifest")):
@@ -82,3 +84,20 @@ class InstallToRokuSettingsPasswordCommand(sublime_plugin.WindowCommand):
 			print "changes not saved"
 			
 		sublime.Window.show_input_panel(sublime.active_window(), "Roku Developer Password", password, on_done, on_change, on_cancel)
+
+class InstallToRokuSettingsPathCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		settings = sublime.load_settings("InstallToRoku.sublime-settings")
+		path = sublime.Settings.get(settings, "roku_channel_path")
+
+		def on_done(user_input):
+			sublime.Settings.set(settings, "roku_channel_path", user_input)
+			sublime.save_settings("InstallToRoku.sublime-settings")
+
+		def on_change(user_input):
+			pass
+
+		def on_cancel():
+			print "changes not saved"
+
+		sublime.Window.show_input_panel(sublime.active_window(), "Roku Channel Path", path, on_done, on_change, on_cancel)
